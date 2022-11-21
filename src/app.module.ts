9268f87f5eb0607408a083a5e77c4820799c1b84
quashima <ex.rwksm1024@gmail.com>
2022-11-21 23:43:37 +0900

    change path

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './adapters/primary/app.controller';
+import { AppService } from './domain/app.service';
 
 @Module({
   imports: [],
