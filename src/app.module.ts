5d07b3596cf0a5c6e2704ea8ba5f879ca8d2577c
quashima <ex.rwksm1024@gmail.com>
2022-11-24 23:39:54 +0900

    delete app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './adapters/primary/app.controller';
 import { UserController } from './adapters/primary/user.controller';
-import { AppService } from './domain/app.service';
 import { UserService } from './domain/user.service';
 import { PrismaAdapter } from './adapters/secondary/prisma.adapter';
 
 @Module({
   imports: [],
-  controllers: [AppController, UserController],
-  providers: [AppService, UserService, PrismaAdapter],
+  controllers: [UserController],
+  providers: [UserService, PrismaAdapter],
 })
 export class AppModule {}
