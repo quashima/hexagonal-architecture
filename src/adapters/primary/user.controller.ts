5dfb5db7dc6db86f5bb509439dded5bbbc9613d5
quashima <ex.rwksm1024@gmail.com>
2022-11-30 23:15:37 +0900

    move directory

--- src/adapters/primary/user.controller.ts
+++ src/adapters/primary/user.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
-import { UserService } from '../../domain/user.service';
+import { UserService } from '../../application/domain/user.service';
 
 @Controller('users')
 export class UserController {
